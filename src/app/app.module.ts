fee7ace9341ef7f47cb935fb2bdfc6a29105eba5
AffanHakak <affanshowkat@gmail.com>
2022-07-13 21:58:36 +0530

    imported highcharts

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ChartModule } from 'angular-highcharts';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, ChartModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
