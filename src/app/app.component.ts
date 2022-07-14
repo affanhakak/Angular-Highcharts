19808320be6ca2d513386b9aa7ac727272e61bcb
Affan Hakak <affanshowkat@gmail.com>
2022-07-14 13:33:18 +0530

    added bar chart, pie chart and area chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Chart } from 'angular-highcharts';
+import { areaChartOptions } from './helpers/areaChartOptions';
+import { barChartOptions } from './helpers/barChartOptions';
+import { donutChartOptions } from './helpers/donutChartOptions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-highcharts';
+  title = 'Angular-Highcharts';
+  donutChart = new Chart(donutChartOptions);
+  areaChart = new Chart(areaChartOptions);
+  barChart = new Chart(barChartOptions)
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ChartModule } from 'angular-highcharts';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule, ChartModule,
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, NgbModule, ChartModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
